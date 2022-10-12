43124338d3eb63b1798dd7c813558909c55a29d8
WiratThiangtham <wirat.thiangtham.wt@gmail.com>
2022-10-12 16:32:19 +0700

    Fix the sign-up page

--- lib/components/already_have_an_account_acheck.dart
+++ lib/components/already_have_an_account_acheck.dart
@@ -22,7 +22,7 @@ class AlreadyHaveAnAccountCheck extends StatelessWidget {
         GestureDetector(
           onTap: press as void Function()?,
           child: Text(
-            login ? "Sign Up" : "Sign In",
+            login ? "Sign Up" : "Log In",
             style: const TextStyle(
               color: kPrimaryColor,
               fontWeight: FontWeight.bold,
